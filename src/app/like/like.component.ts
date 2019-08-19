ce5ef4232224ba43c61c3150ce85b1fab99a0548
chris <christianwilliamsjire@gmail.com>
2019-08-19 16:15:29 +0300

    this commit will fic like button again

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -9,11 +9,11 @@ export class LikeComponent implements OnInit{
 
   numberOfLikes : number = 0;
   
-  likeButtonClick(id) {
+  likeButtonClick() {
     this.numberOfLikes++;
   }
 
-  dislikeButtonClick(id) {
+  dislikeButtonClick() {
     this.numberOfLikes--;
   }
 
